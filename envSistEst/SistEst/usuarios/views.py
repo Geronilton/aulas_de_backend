8980b00fc340570bab6f5e1eb8b3f57a7423066d
geronilton <geronilton001@gmail.com>
2023-07-04 15:39:08 -0300

    add metodo de envio de formulario

--- envSistEst/SistEst/usuarios/views.py
+++ envSistEst/SistEst/usuarios/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForms
+from django.http import Http404
+from django.http import HttpRequest
 
 
 # Create your views here.
 def register_view(request):
+
+    # num_visitas = request.session.get("num_visitas", 0)
+    # request.session["num_visitas"] = num_visitas + 1
+
+    register_form = request.session.get("register_form",None )
+
+    form = RegisterForms(register_form)
+
+    form = RegisterForms()
+    return render(request, 'pages/register_view.html', {'form': form})
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+
+    post = request.POST
+    request.session["register_form"] = post
+
+    return redirect('usuarios:register')
+
     form = RegisterForms()
     return render(request, 'pages/register_view.html', {'form': form})
